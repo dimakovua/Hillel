0db7a07fd6838813a6140ad6d28176b3d6cd140c
kate <kate@localhost.localdomain>
2024-09-21 00:49:31 +0300

    Added gitignore file and little changes in main

--- Lesson-4/main.cpp
+++ Lesson-4/main.cpp
@@ -1,30 +1,36 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <vector>
 
-std::mutex mtx;
+std::mutex mtx; 
+int chicken_count = 0; 
+int egg_count = 0; 
 
-void egg() {
-    
-    std::lock_guard<std::mutex> lock (mtx);
-    std::cout << "Egg!" << std::endl;
-}
-
-void chicken() {
-    std::lock_guard<std::mutex> lock(mtx);
-    std::cout << "Chicken!" << std::endl;
+void run(const std::string& message, int& counter) {
+    for (int i = 0; i < 10; ++i) {
+        std::lock_guard<std::mutex> lock(mtx);
+        
+        counter++;
+        
+        std::cout << message << " " << counter << std::endl;
+    }
 }
 
 int main() {
-    
-    std::thread t1(egg);
-    std::thread t2(chicken);
+    std::vector<std::thread> threads;
 
-    t1.join();
-    t2.join();
+    for (int i = 0; i < 5; ++i) {
+        threads.emplace_back(run, "Chicken", std::ref(chicken_count));
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        threads.emplace_back(run, "Egg", std::ref(egg_count));
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
 
     return 0;
-
 }
-
-
